4c75e4f709914b2df3fc998986135393a884da9d
Patryk Futa <patryk.futa@gmail.com>
2021-12-02 18:51:16 +0100

    Add Kubernetes provider

--- main.tf
+++ main.tf
@@ -91,12 +91,6 @@ data "aws_eks_cluster_auth" "eks" {
   name = module.eks.cluster_id
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
-
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
 
@@ -126,3 +120,10 @@ module "eks" {
     Terraform = "true"
   }
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+

2902461f7e861c2debc0d489cc2ca55d483e06cb
Patryk Futa <patryk.futa@gmail.com>
2021-12-02 18:27:13 +0100

    Define vpc and k8s modules

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     ]  
   }
 }
-resource "aws_security_group" "all_worker_mggmt" {
+resource "aws_security_group" "all_worker_mgmt" {
   name_prefix = "all_worker_management"
   vpc_id = module.vpc.vpc_id
 
@@ -51,5 +51,78 @@ resource "aws_security_group" "all_worker_mggmt" {
       "172.16.0.0/12",
       "192.168.0.0/16",
     ]
+  }
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "terraform-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway   = true
+  enable_vpn_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb" = 1
+  }
+
+  tags = {
+    Terraform = "true"
+  }
+}
+
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+
+  cluster_version                 = "1.21"
+  cluster_name                    = var.cluster_name
+  vpc_id                          = module.vpc.vpc_id
+  subnets                         = module.vpc.private_subnets
+  cluster_create_timeout          = "1h"
+  cluster_endpoint_private_access = true
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.micro"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      asg_desired_capacity          = 1
+    }
+  ]
+
+  worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  map_roles                            = var.map_roles
+  map_users                            = var.map_users
+  map_accounts                         = var.map_accounts
+
+  tags = {
+    Terraform = "true"
   }
 }

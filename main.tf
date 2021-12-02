1237601a29cbc824df185dabcf34fe95958fc763
Patryk Futa <patryk.futa@gmail.com>
2021-12-02 19:22:30 +0100

    Add kubernetes deployment

--- main.tf
+++ main.tf
@@ -142,3 +142,48 @@ resource "kubernetes_service" "example" {
     type = "LoadBalancer"
   }
 }
+
+resource "kubernetes_deployment" "example" {
+  metadata {
+    name = "terraform-example"
+    labels = {
+      test = "MyExampleApp"
+    }
+  }
+  
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+    spec {
+      container {
+        image = "nginx:latest"
+        name = "example"
+
+        resources {
+          limits = {
+            cpu = "0.5"
+            memory = "512Mi"
+          }
+          requests = {
+            cpu = "250m"
+            memory = "50Mi"
+          }
+        }
+      }
+    }
+    }
+  }
+}

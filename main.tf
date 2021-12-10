44b16eff34297fc49dde1107ae97a0fbbbd4c30c
Patryk Futa <patryk.futa@gmail.com>
2021-12-10 11:25:45 +0100

    HPA with behavior scaling

--- main.tf
+++ main.tf
@@ -181,3 +181,52 @@ resource "kubernetes_service" "nginx-service" {
     type = "LoadBalancer"
   }
 }
+
+# HPA with behavior autoscaling
+resource "kubernetes_horizontal_pod_autoscaler" "example" {
+  metadata {
+    name = "nginx-hpa"
+  }
+
+  spec {
+    min_replicas = 2
+    max_replicas = 4
+
+    scale_target_ref {
+      kind = "Deployment"
+      name = "TheExampleApp"
+    }
+
+    behavior {
+      scale_down {
+        stabilization_window_seconds = 300
+        select_policy                = "Min"
+        policy {
+          period_seconds = 120
+          type           = "Pods"
+          value          = 1
+        }
+
+        policy {
+          period_seconds = 310
+          type           = "Percent"
+          value          = 100
+        }
+      }
+      scale_up {
+        stabilization_window_seconds = 600
+        select_policy                = "Max"
+        policy {
+          period_seconds = 180
+          type           = "Percent"
+          value          = 100
+        }
+        policy {
+          period_seconds = 600
+          type           = "Pods"
+          value          = 5
+        }
+      }
+    }
+  }
+}
